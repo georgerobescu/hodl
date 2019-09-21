d39fff59be0d48a5517503a29ff2914b14216922
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-09-20 23:56:52 -0700

    git clone sass-boilerplate and create nav

--- compile.js
+++ compile.js
@@ -1,8 +1,58 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const solc = require('solc');
 
-const contractPath = path.resolve(__dirname, 'contracts', 'Hodl.sol');
-const source = fs.readFileSync(contractPath, 'utf8');
+const buildPath = path.resolve('build');
 
-module.exports = solc.compile(source, 1).contracts[':Hodl'];
+const createBuildFolder = () => {
+  fs.emptyDirSync(buildPath);
+};
+
+const contractFolderPath = path.resolve('contracts');
+
+const buildSources = () => {
+  const sources = {};
+  const contractFiles = fs.readdirSync(contractFolderPath);
+
+  contractFiles.forEach(file => {
+    const contractFullPath = path.resolve(contractFolderPath, file);
+    sources[file] = {
+      content: fs.readFileSync(contractFullPath, 'utf8')
+    };
+  });
+
+  return sources;
+}
+
+const input = {
+  language: 'Solidity',
+  sources: buildSources(),
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['abi', 'evm.bytecode']
+      }
+    }
+  }
+}
+
+const compileContracts = () => {
+  const compiledContracts = JSON.parse(solc.compile(JSON.stringify(input))).contracts;
+
+  for(let contract in compiledContracts) {
+    for(let contractName in compiledContracts[contract]) {
+      fs.outputJSONSync(
+        path.resolve(buildPath, `${contractName}.json`),
+        compiledContracts[contract][contractName],
+        {
+          spaces: 2
+        }
+      )
+    }
+  }
+}
+
+(function run() {
+  createBuildFolder();
+  compileContracts();
+})();
