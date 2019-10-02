f8ae0b247cc437b1e93cffb1c99670ee7e447911
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-10-01 20:39:32 -0700

    Implement time units and conditional rendering

--- client/src/components/Hodler.js
+++ client/src/components/Hodler.js
@@ -3,11 +3,60 @@ import React, {Component} from 'react';
 import '../layout/components/hodler.sass';
 
 class Hodler extends Component {
-  getTime(earliest, now) {
-    if(earliest < now) {
-      return 0;
+  state = {
+    date: null
+  };
+
+  componentDidMount() {
+    this.getTime();
+  }
+
+  getTime() {
+    if(this.props.user.earliest < Math.floor(Date.now() / 1000)) {
+      this.setState({date: 0});
     } else {
-      return earliest - now;
+      return this.formatTime(this.props.user.earliest - Math.floor(Date.now() / 1000));
+    }
+  }
+
+  formatTime(time) {
+    const date = new Date(time * 1000);
+    const days = date.getUTCDate() - 1;
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getSeconds();
+    this.setState({
+      date: {
+        days, 
+        hours,
+        minutes,
+        seconds
+      }
+    });
+  }
+
+  renderTime() {
+    if(this.state.date != null && this.state.date !== 0) {
+      return(
+        <div className="hodler__group">
+          <p className="hodler__name">
+            Time left:
+          </p>
+          <p className="hodler__value">
+            <span>{this.state.date.days}</span> days <span>{this.state.date.hours}</span> hours <span>{this.state.date.minutes}</span> minutes <span>{this.state.date.seconds}</span> seconds
+          </p>
+        </div>
+      );
+    }
+  }
+
+  renderButton() {
+    if(this.state.date === 0) {
+      return(
+        <button className="hodler__button">
+          Withdraw
+        </button>
+      );
     }
   }
 
@@ -22,18 +71,8 @@ class Hodler extends Component {
             <span>{this.props.web3.utils.fromWei(this.props.user.amount, 'ether')}</span> eth
           </p>
         </div>
-        <div className="hodler__group">
-          <p className="hodler__name">
-            Time left:
-          </p>
-          <p className="hodler__value">
-            {/* Add proper units */}
-            <span>{this.getTime(this.props.user.earliest, Math.floor(Date.now() / 1000))}</span> seconds
-          </p>
-        </div>
-        <button className="hodler__button">
-          Withdraw
-        </button>
+        {this.renderTime()}
+        {this.renderButton()}
       </section>
     )
   }
