47ca6896f9359ec3f7fbc4b909e070a57ed37eba
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-10-02 16:23:46 -0700

    Updates

---
+++ precache-manifest.02a0e6c1ddffd20a8c17710dbfa28d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f83354a3ddac178e568a26d51b4130e8",
+    "url": "/hodl/static/media/github.f83354a3.png"
+  },
+  {
+    "revision": "6fb8a048189dd72452ba",
+    "url": "/hodl/static/js/runtime~main.6fb8a048.js"
+  },
+  {
+    "revision": "dd51355ba282ce57ec0d",
+    "url": "/hodl/static/js/main.dd51355b.chunk.js"
+  },
+  {
+    "revision": "f2f35100ae0fcc9ad2a8",
+    "url": "/hodl/static/js/1.f2f35100.chunk.js"
+  },
+  {
+    "revision": "dd51355ba282ce57ec0d",
+    "url": "/hodl/static/css/main.cdc4b6b5.chunk.css"
+  },
+  {
+    "revision": "e5982b1e8f7c4c89caca1d725c88783d",
+    "url": "/hodl/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.350bf33bf7f443912611148f9bd86bbc.js"
+  "/hodl/precache-manifest.02a0e6c1ddffd20a8c17710dbfa28d0d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hodl/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
