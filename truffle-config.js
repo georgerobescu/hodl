ae74a0706d2076ad0750b7ee9d67d150c21b1ee7
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-10-02 14:07:07 -0700

    Launch on testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const {mnemonic} = require('./keys');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +9,12 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/16ecd43deacb4e5f8410126db8706727");
+      },
+      network_id: '3',
     }
   }
 };
