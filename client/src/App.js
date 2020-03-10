88b41eba9d03c100a07d0d0ae2d858cfe04a5510
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-10 13:34:16 -0700

    Remove no web3 alert

--- client/src/App.js
+++ client/src/App.js
@@ -47,10 +47,6 @@ class App extends Component {
       // example of interacting with the contract's methods.
       this.setState({ web3, account, contract: instance });
     } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
       console.error(error);
     }
 
